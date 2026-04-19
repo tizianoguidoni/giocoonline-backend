df3075ff49841389ce21d7e42a33412f598b1d23
tizianoguidoni <tizianoguidoni@gmail.com>
2026-04-19 17:17:54 +0200

    fix: allow all CORS origins for Vercel-Railway cross-origin communication

--- main.py
+++ main.py
@@ -1852,7 +1852,7 @@ app.include_router(api_router)
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
